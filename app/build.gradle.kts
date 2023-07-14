0824d62ba0284f01bb34b77d71c52abe3d666b31
Youngjik Yoon <laco2951@gmail.com>
2023-07-14 16:57:18 +0900

    groovy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,13 +3,15 @@ plugins {
     kotlin("android")
 }
 
+apply(from = "${rootDir}/shared/gradle/common/common.android.application.gradle")
+apply(from = "${rootDir}/shared/gradle/common/common.android.compose.gradle")
+apply(from = "${rootDir}/shared/gradle/common/common.android.hilt.gradle")
+
 android {
     namespace = "com.laco.gradle.conventions"
-    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.laco.gradle.conventions"
-        minSdk = 24
         versionCode = 1
         versionName = "1.0"
 
@@ -17,34 +19,6 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.majorVersion
-    }
-
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
     }
 }
 
@@ -58,22 +32,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtimeCompose)
     implementation(libs.androidx.activity.compose)
-
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-
+    
     testImplementation(libs.junit4)
-
     androidTestImplementation(libs.androidx.test.ext)
     androidTestImplementation(libs.androidx.test.espresso.core)
-
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.testManifest)
-
-    coreLibraryDesugaring(libs.android.desugarJdkLibs)
 }
--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,45 +1,10 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("com.android.library")
     kotlin("android")
 }
 
+apply(from = "${rootDir}/shared/gradle/common/common.android.library.gradle")
+
 android {
     namespace = "com.laco.gradle.conventions.core.data"
-    
-    compileSdk = 33
-    defaultConfig {
-        minSdk = 24
-    }
-    
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            consumerProguardFiles("proguard-rules.pro")
-        }
-    }
-    
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
-    }
-    
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_17.majorVersion
-        }
-    }
-}
-
-dependencies {
-    coreLibraryDesugaring(libs.android.desugarJdkLibs)
 }
